f73adbce262577ca8fb82382e65997aecd9c40a4
Manan Karnik <manankarnik03@gmail.com>
2023-03-16 21:37:12 +0530

    build: add tailwindcss

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use yew::prelude::*;
 fn app() -> Html {
     html! {
         <main>
-            <h1>{"Hello World!"}</h1>
+            <h1 class="text-3xl font-bold underline">{"Hello World!"}</h1>
         </main>
     }
 }
