a3e9c9d6cd7c87c6997dd63db592ad8b22893445
Manan Karnik <manankarnik03@gmail.com>
2023-03-16 21:20:03 +0530

    feat: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <main>
+            <h1>{"Hello World!"}</h1>
+        </main>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
